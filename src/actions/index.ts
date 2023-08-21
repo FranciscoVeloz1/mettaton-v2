b3f3c908b203987e8988f172bb677baa35d19750
FranciscoVeloz <franciscoveloz245@gmail.com>
2023-08-21 16:22:36 -0600

    feat: added map instead for

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,17 +1,17 @@
-import fs from "fs/promises";
+import fs from 'fs/promises'
 
-import { lexicon } from "./lexicon";
+import { lexicon } from './lexicon'
 
 export const readFile = async (file: string) => {
   try {
-    const split = file.split(".");
-    if (!file.includes(".mtt") || split[split.length - 1] !== "mtt")
-      throw new Error("File extension must be .mtt");
+    const split = file.split('.')
+    if (!file.includes('.mtt') || split[split.length - 1] !== 'mtt') throw new Error('File extension must be .mtt')
 
-    const data = await fs.readFile(file, "utf8");
-    const splitted = data.split(/[\n]+/);
-    lexicon(splitted);
+    const data = await fs.readFile(file, 'utf8')
+    const splitted = data.split(/[\n]+/)
+    const lex = lexicon(splitted)
+    console.table(lex)
   } catch (error) {
-    if (error instanceof Error) console.error(error.message);
+    if (error instanceof Error) console.error(error.message)
   }
-};
+}
--- src/actions/lexicon.ts
+++ src/actions/lexicon.ts
@@ -1,28 +1,29 @@
-import { regularExpresions } from "./regex";
-import { ILexicon } from "../interfaces/ILexicon";
+import { regularExpresions } from './regex'
+import { ILexicon } from '../interfaces/ILexicon'
 
 export const lexicon = (data: string[]) => {
-  const result: ILexicon[] = [];
+  const result: ILexicon[] = data
+    .map((token, i) => {
+      const trimmedToken = token.trim()
+      const matchedRegex = regularExpresions.find((r) => r.regex.test(trimmedToken))
 
-  for (let i = 0; i < data.length; i++) {
-    const token = data[i];
-
-    for (let j = 0; j < regularExpresions.length; j++) {
-      const r = regularExpresions[j];
-
-      const comparison = r.regex.test(token.trim());
-      if (comparison) {
-        result.push({
+      if (matchedRegex) {
+        return {
           num: i,
-          token: token.trim(),
-          description: r.title,
-          line: i,
-        });
-
-        j = regularExpresions.length + 1;
+          token: trimmedToken,
+          description: matchedRegex.title,
+          line: i
+        }
       }
-    }
-  }
 
-  console.table(result);
-};
+      return {
+        num: i,
+        token: trimmedToken,
+        description: 'unknown',
+        line: i
+      }
+    })
+    .filter((result) => result !== null)
+
+  return result
+}
